65cff044156457533021c1c79d71eb3d5e2c1fad
harryssuperman <harryssuperman@hotmail.com>
2023-04-02 04:00:53 +0200

    Update App.java

--- src/main/java/com/github/harryssuperman/fxdatepicker/App.java
+++ src/main/java/com/github/harryssuperman/fxdatepicker/App.java
@@ -2,31 +2,16 @@ package com.github.harryssuperman.fxdatepicker;
 
 import java.time.LocalDate;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.DatePicker;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
 /**
  * Hello world!
  *
  */
-public class App extends Application
+public class App 
 {
     public static void main( String[] args )
     {
-        launch(args);
+        System.out.println("f");
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        DatePicker datepicker = new DatePicker(LocalDate.now());
-        StackPane stackPane = new StackPane(datepicker);
-        Scene scene = new Scene(stackPane);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }
 }
